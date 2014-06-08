0f1f8734d984033487a96d5a15ddbbb80d1eb1ef
thks10 <softinus@users.noreply.github.com>
2014-06-08 13:13:50 +0000

    -XML 구조 (80% 완성)

--- Songpa/songpa/manager_pc/RushTicketContent.xaml.cs
+++ Songpa/songpa/manager_pc/RushTicketContent.xaml.cs
@@ -35,40 +35,50 @@ namespace manager_pc
             this.currentName = _currentName;
             this.isNewBoard = _isNewBoard;
 
-            if (isNewBoard) // 영문제목을 폴더명으로 쓰기 떄문에 수정시 변경 할 수 없다. [6/8/2014 Mark]
-                txt_EngTitle.IsEnabled = true;
-            else
-                txt_EngTitle.IsEnabled = false;
-
             rkey = Registry.CurrentUser.OpenSubKey("SONGPA").OpenSubKey("root_info", true);
 
             if (rkey.GetValue("PATH") != null)
                 pathRushRoot = rkey.GetValue("PATH").ToString() + "\\rush_ticket_root";
+
+            if (isNewBoard) // 영문제목을 폴더명으로 쓰기 떄문에 수정시 변경 할 수 없다. [6/8/2014 Mark]
+            {
+                txt_EngTitle.IsEnabled = true;
+            }
+            else
+            {
+                txt_EngTitle.IsEnabled = false;
+                XMLLoad(pathRushRoot + "\\" + currentName);
+            }
         }
 
         private void btn_SelImage_Click(object sender, RoutedEventArgs e)
         {
-
         }
 
         private void btn_Save_Click(object sender, RoutedEventArgs e)
         {
-            if (isNewBoard) // new or modify? [6/8/2014 Mark]
-            {
-                currentName = txt_EngTitle.Text;
-                DirectoryInfo di_SUB = new DirectoryInfo(pathRushRoot + "\\" + currentName);  //Create Directoryinfo value by sDirPath
+            //if (isNewBoard) // new or modify? [6/8/2014 Mark]
+            //{
 
-                if (di_SUB.Exists == false) // 해당 이름의 폴더가 존재하지 않으면... [6/8/2014 Mark]
-                {
-                    di_SUB.Create();
-                    XMLCreate(di_SUB.FullName);
-                }
-                Close();
-            }
-            else // modify [6/8/2014 Mark]
+            //}
+            //else // modify [6/8/2014 Mark]
+            //{
+            //    Close();
+            //}
+
+            currentName = txt_EngTitle.Text;
+            DirectoryInfo di_SUB = new DirectoryInfo(pathRushRoot + "\\" + currentName);  //Create Directoryinfo value by sDirPath
+
+            if (di_SUB.Exists == false) // 해당 이름의 폴더가 존재하지 않으면... [6/8/2014 Mark]
             {
-                Close();
+                di_SUB.Create();
+                XMLCreate(di_SUB.FullName);
             }
+            else
+            {
+                XMLCreate(di_SUB.FullName);
+            }
+            Close();
         }
 
         private void btn_Cancel_Click(object sender, RoutedEventArgs e)
@@ -79,16 +89,90 @@ namespace manager_pc
 
         private void XMLCreate(String pathToSave)
         {
-            // 문서를 만들고 지정된 값의 노드를 만든다..
-            XmlDocument NewXmlDoc = new XmlDocument();
-            //NewXmlDoc.AppendChild(NewXmlDoc.CreateXmlDeclaration("1.0", "utf-8", "yes"));
+            // 생성할 XML 파일 경로와 이름, 인코딩 방식을 설정합니다.
+            XmlTextWriter textWriter = new XmlTextWriter(pathToSave + "\\1.xml", Encoding.UTF8);
 
-            // 최상위 노드를 만든다.
-            XmlNode root = NewXmlDoc.CreateElement("", "Root", "");
-            NewXmlDoc.AppendChild(root);
+            // 들여쓰기 설정
+            textWriter.Formatting = Formatting.Indented;
 
-            // 지정된 XML문서로 만들고 저장한다.
-            NewXmlDoc.Save(pathToSave + "\\1.xml");
+            // 문서에 쓰기를 시작합니다.
+            textWriter.WriteStartDocument();
+
+            // 루트 설정
+            textWriter.WriteStartElement("root");
+            {
+                // 노드와 값 설정
+                AddElement(textWriter, "title_eng", txt_EngTitle.Text);
+                AddElement(textWriter, "title_kor", txt_KorTitle.Text);
+                AddElement(textWriter, "pos_eng", txt_EngPosition.Text);
+                AddElement(textWriter, "pos_kor", txt_KorPosition.Text);
+                AddElement(textWriter, "period_eng", txt_EngPeriod.Text);
+                AddElement(textWriter, "period_kor", txt_KorPeriod.Text);
+                AddElement(textWriter, "time_eng", txt_EngTime.Text);
+                AddElement(textWriter, "time_kor", txt_KorTime.Text);
+                AddElement(textWriter, "price_eng", txt_EngPrice.Text);
+                AddElement(textWriter, "price_kor", txt_KorPrice.Text);
+                AddElement(textWriter, "contact", txt_Contact.Text);
+            }
+            textWriter.WriteEndElement();
+
+            textWriter.WriteEndDocument();
+            textWriter.Close();
+        }
+
+        private void AddElement(XmlTextWriter w, String key, String text)
+        {
+            w.WriteStartElement(key);
+            w.WriteString(text);
+            w.WriteEndElement();
+        }
+
+        private void XMLLoad(String pathToLoad)
+        {
+            XmlDocument XmlDoc = new XmlDocument();
+            XmlDoc.Load(pathToLoad + "\\1.xml");
+            XmlNode root= XmlDoc.DocumentElement;
+            XmlNodeList list = root.ChildNodes;
+
+            foreach(XmlNode node in list)
+            {
+                switch (node.Name)
+                {
+                    case "title_eng":
+                        txt_EngTitle.Text = node.InnerText;
+                        break;
+                    case "title_kor":
+                        txt_KorTitle.Text = node.InnerText;
+                        break;
+                    case "pos_eng":
+                        txt_EngPosition.Text = node.InnerText;
+                        break;
+                    case "pos_kor":
+                        txt_KorPosition.Text = node.InnerText;
+                        break;
+                    case "period_eng":
+                        txt_EngPeriod.Text = node.InnerText;
+                        break;
+                    case "period_kor":
+                        txt_KorPeriod.Text = node.InnerText;
+                        break;
+                    case "time_eng":
+                        txt_EngTime.Text = node.InnerText;
+                        break;
+                    case "time_kor":
+                        txt_KorTime.Text = node.InnerText;
+                        break;
+                    case "price_eng":
+                        txt_EngPrice.Text = node.InnerText;
+                        break;
+                    case "price_kor":
+                        txt_KorPrice.Text = node.InnerText;
+                        break;
+                    case "contact":
+                        txt_Contact.Text = node.InnerText;
+                        break;
+                }
+            }
         }
     }
 }
