1d7e30b587af869ba413a5a7f7487961d5d89441
thks10 <softinus@users.noreply.github.com>
2014-09-10 11:22:33 +0000

    Chinese IME

---
+++ CIME/CIME/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("새굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(564, 400);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            this.richTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 421);
+            this.Controls.Add(this.richTextBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 460);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 460);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chinese IME Prototype";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+
+    }
+}
+
---
+++ CIME/CIME/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Data.OleDb;
+using System.Collections;
+
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ReadExcelfile(@"P01.xlsx");
+
+            if (arrCode.Count == 0)
+            {
+                MessageBox.Show("데이터 파일을 찾을 수 없거나, 잘못된 파일입니다.");
+                Application.Exit();
+            }
+        }
+
+        bool bCtrl = false;
+
+        String word = "";
+        String word_completed = "";
+
+        List<String> arrCode = new List<String>();
+        List<String> arrData = new List<String>();
+
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 32)   // 스페이스 || 엔터
+            {
+                //MessageBox.Show(word);
+                word_completed += FindChineseWord();
+                word = "";
+
+                richTextBox1.Text = word_completed;
+                e.KeyChar = Convert.ToChar(0);
+                richTextBox1.Select(richTextBox1.Text.Length, 0);
+            }
+            else if (e.KeyChar == 8 || e.KeyChar == 13 || e.KeyChar == 27)    // 백스페이스
+            {
+                    e.KeyChar = Convert.ToChar(0);
+                    richTextBox1.Text = word_completed;
+                    richTextBox1.Select(richTextBox1.Text.Length, 0);
+                    word = "";
+            }
+            else if (e.KeyChar >= 65 || e.KeyChar <= 90)
+            {        
+                if (!bCtrl)
+                {
+                word += e.KeyChar;
+                e.KeyChar = Convert.ToChar(46);
+                }
+            }
+            else
+            {
+                e.KeyChar = Convert.ToChar(0);
+            }
+        }
+
+
+        private string FindChineseWord()
+        {
+            for (int i = 0; i < arrCode.Count; ++i )
+            {
+                if (word.ToUpper() == arrCode[i].ToUpper())   // 맞는 코드를 찾으면...
+                {
+                    return arrData[i];
+                }
+            }
+
+            return "";
+
+        }
+
+
+        private void ReadExcelfile(String strFilePath)
+        {
+            try
+            {
+            // OLEDB를 이용한 엑셀 연결
+            string szConn = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + strFilePath + @";Extended Properties=Excel 12.0";
+
+            OleDbConnection conn = new OleDbConnection(szConn);
+            conn.Open();
+
+            // 엑셀로부터 데이타 읽기
+            OleDbCommand cmd = new OleDbCommand("SELECT * FROM [Sheet1$]", conn);
+            OleDbDataAdapter adpt = new OleDbDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adpt.Fill(ds);
+
+            
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    if(dr[2] == DBNull.Value)
+                        break;
+
+                    String code = (String)dr[2];
+                    String data = (String)dr[3];
+                    if (code.Trim() == "")
+                        continue;
+
+
+                    arrCode.Add(code);
+                    arrData.Add(data);
+
+                    //string data = string.Format("CODE:{0}, DATA:{1}", dr[2], dr[3]);
+                    //MessageBox.Show(data);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("err" + ex.ToString()); 
+            }
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control)
+            {
+                bCtrl = true;
+            }
+        }
+
+        private void richTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Control)
+            {
+                bCtrl = false;
+            }
+        }
+
+    }
+}
